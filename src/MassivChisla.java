68f4f8f420ef64598b184939216de735c6bb7c93
Alyona4433 <alena4433@gmail.com>
2023-08-14 20:47:16 +0200

    Changes 2

--- src/MassivChisla.java
+++ src/MassivChisla.java
@@ -1,32 +1,20 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class MassivChisla {
 
-    public static String processNumberLists(List<String> inputList) {
-        List<Integer> numbers = new ArrayList<>();
-
-        for (String str : inputList) {
-            String[] numStrings = str.split(", ");
-            for (String numStr : numStrings) {
-                numbers.add(Integer.parseInt(numStr));
-            }
-        }
-
-        List<Integer> sortedNumbers = numbers.stream()
+    public static String processNumberArrays(String[] inputArray) {
+        return Arrays.stream(inputArray)
+                .flatMap(str -> Arrays.stream(str.split(", ")))
+                .map(Integer::parseInt)
                 .sorted()
-                .collect(Collectors.toList());
-
-        return sortedNumbers.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining(", "));
     }
 
     public static void main(String[] args) {
-        List<String> inputList = Arrays.asList("1, 2, 0", "4, 5");
-        String result = processNumberLists(inputList);
+        String[] inputArray = {"1, 2, 0", "4, 5"};
+        String result = processNumberArrays(inputArray);
         System.out.println(result);
     }
 }
