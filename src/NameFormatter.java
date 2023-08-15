d8b4d8c48b224ae69fe0cdeb97863a64432e2bf4
Alyona4433 <alena4433@gmail.com>
2023-08-15 18:54:16 +0200

    Changes 4

--- src/NameFormatter.java
+++ src/NameFormatter.java
@@ -1,25 +1,16 @@
-import java.util.List;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class NameFormatter {
-
-    public static String formatNames(List<String> names) {
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < names.size(); i++) {
-            if (i % 2 == 0) {
-                result.append((i + 1)).append(". ").append(names.get(i));
-                if (i < names.size() - 2) {
-                    result.append(", ");
-                }
-            }
-        }
-
-        return result.toString();
-    }
-
     public static void main(String[] args) {
-        List<String> names = List.of("Ivan", "John", "Peter", "Mary", "Alice", "Michael");
-        String formattedNames = formatNames(names);
-        System.out.println(formattedNames);
+        String[] names = new String[]{"Ivan", "John", "Peter", "Mary", "Alice", "Michael"};
+        formatNames(names);
+    }
+    public static void formatNames(String[] names) {
+        System.out.println(Arrays.stream(names)
+                .map(String::toUpperCase)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining(", ")));
     }
 }
